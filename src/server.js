30361c332bfc73e593ea73dc705ef80a4003a250
selva <jawadhuhills380@gmail.com>
2026-03-26 19:30:19 +0530

    basic

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log("DB Connected");
     app.listen(process.env.PORT, () => {
-      console.log("Server running on port " + process.env.PORT);
+      console.log("Server running on port is " + process.env.PORT);
     });
   })
   .catch(err => console.log(err));

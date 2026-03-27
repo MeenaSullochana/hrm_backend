5e44ea32c568abd906d194e16d0ed0898c810ceb
selva <jawadhuhills380@gmail.com>
2026-03-27 11:32:20 +0530

    basic

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
+app.use(cors({
+    origin: [
+      'http://localhost:5173',   // React local
+      'https://your-frontend.onrender.com' // deployed frontend
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/roles", require("./routes/roleRoutes"));
 app.use("/api/employees", require("./routes/employeeRoutes"));

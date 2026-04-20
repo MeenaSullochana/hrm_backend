906d20e7a7a817f024cf25cf47d561d9e4e668a8
meenakshi-crud <meenakshi.sullochana@crud-operations.com>
2026-04-20 18:33:57 +0530

    test

--- src/models/Company.js
+++ src/models/Company.js
@@ -13,6 +13,16 @@ const companySchema = new mongoose.Schema({
     type: String,
     required: true // e.g. ABC
   },
+    codeConfig: {
+    digits: {
+      type: Number,
+      default: 5
+    },
+    startFrom: {
+      type: Number,
+      default: 1
+    }
+  },
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
